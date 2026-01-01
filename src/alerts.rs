2b4b69a44187277d4f5bd464aa22d77f40a838bb
W4lk1nB0n3s <bloodlygauntlet@gmail.com>
2026-01-01 12:21:26 -0800

    Update alerts.rs

--- src/alerts.rs
+++ src/alerts.rs
@@ -3,20 +3,23 @@ use rodio::{Decoder, OutputStreamBuilder, Sink};
 use std::io::Cursor;
 
 #[derive(Embed)]
-#[folder = "File\path\to\your.mp3"] 
+#[folder = "assets"] 
 struct Asset;
 
 pub fn trigger_timer_end(_ctx: &egui::Context) {
     // 1. Initialize the output stream using the new Builder API
-    let stream_handle = OutputStreamBuilder::open_default_stream()
+    // Replacing OutputStream::try_default()
+    let stream = 
+    OutputStreamBuilder::open_default_stream()
         .expect("Failed to open default output stream");
     
     // 2. Initialize the sink using the mixer from the stream handle
-    let sink = Sink::connect_new(stream_handle.mixer());
+    // Replacing Sink::try_new(&handle)
+    let sink = Sink::connect_new(stream.mixer());
 
     // 3. Load embedded file
-    let embedded_file = Asset::get("File\path\to\your.mp3")
-        .expect("rickrolll.mp3 not found in assets/");
+    let embedded_file = Asset::get("???.mp3")  // replace the ??? with the name of the .mp3
+        .expect("???.mp3 not found in assets");
 
     // 4. Wrap bytes in Cursor for decoding
     let cursor = Cursor::new(embedded_file.data);
@@ -28,6 +31,6 @@ pub fn trigger_timer_end(_ctx: &egui::Context) {
     
     // Note: The stream_handle must remain in scope for playback to continue
     sink.sleep_until_end();
-
+    
 
 }
